ecd1443783cfd4d7d2b0fae80fdbea364c77735c
Dmitry Shakun <smilecraft1995@gmail.com>
2019-10-22 19:23:25 +0300

    Minor gulp js code style refactor

--- config.js
+++ config.js
@@ -5,61 +5,63 @@ const production = util.env.production || util.env.prod || false;
 const destPath = util.env.dest || 'build';
 
 const config = {
-    env       : 'development',
-    production: production,
+  env: 'development',
+  production: production,
 
-    src: {
-        root         : 'src',
-        templates    : 'src/pages',
-        templatesData: 'src/templates/data',
-        pagelist     : 'src/index.yaml',
-        sass         : 'src/sass',
-        js           : 'src/js',
-        img          : 'src/img',
-        svg          : 'src/img/svg',
-        icons        : 'src/icons',
-        iconsSvg     : 'src/icons',
-        fonts        : 'src/fonts',
-        lib          : 'src/lib',
-        data         : 'src/data'
-    },
-    dest: {
-        root : destPath,
-        html : destPath,
-        css  : destPath + '/css',
-        js   : destPath + '/js',
-        img  : destPath + '/img',
-        fonts: destPath + '/fonts',
-        lib  : destPath + '/lib',
-        data : destPath + '/data'
-    },
-    addAssets: {
-      'src/fonts/*.{woff,woff2}': '/fonts'
-    },
+  src: {
+    root: 'src',
+    templates: 'src/pages',
+    templatesData: 'src/templates/data',
+    pagelist: 'src/index.yaml',
+    sass: 'src/sass',
+    js: 'src/js',
+    img: 'src/img',
+    svg: 'src/img/svg',
+    icons: 'src/icons',
+    iconsSvg: 'src/icons',
+    fonts: 'src/fonts',
+    lib: 'src/lib',
+    data: 'src/data'
+  },
+  dest: {
+    root: destPath,
+    html: destPath,
+    css: `${destPath}/css`,
+    js: `${destPath}/js`,
+    img: `${destPath}/img`,
+    fonts: `${destPath}/fonts`,
+    lib: `${destPath}/lib`,
+    data: `${destPath}/data`
+  },
+  addAssets: {
+    'src/fonts/*.{woff,woff2}': '/fonts'
+  },
 
-    setEnv: function(env) {
-      if (typeof env !== 'string') return;
-      this.env = env;
-      this.production = env === 'production';
-      process.env.NODE_ENV = env;
-    },
+  setEnv: function(env) {
+    if (typeof env !== 'string') return;
+    this.env = env;
+    this.production = env === 'production';
+    process.env.NODE_ENV = env;
+  },
 
-    logEnv: function() {
-      util.log(
-        'Environment:',
-        util.colors.white.bgRed(' ' + process.env.NODE_ENV + ' ')
-      );
-    },
+  logEnv: function() {
+    util.log(
+      'Environment:',
+      util.colors.white.bgRed(' ' + process.env.NODE_ENV + ' ')
+    );
+  },
 
-    errorHandler: function() {
-      const args = Array.prototype.slice.call(arguments);
-      notify.onError({
-          title: 'Compile Error',
-          message: '<%= error.message %>',
-          sound: 'Submarine'
-      }).apply(this, args);
-      this.emit('end');
-    }
+  errorHandler: function() {
+    const args = Array.prototype.slice.call(arguments);
+    notify
+      .onError({
+        title: 'Compile Error',
+        message: '<%= error.message %>',
+        sound: 'Submarine'
+      })
+      .apply(this, args);
+    this.emit('end');
+  }
 };
 
 config.setEnv(production ? 'production' : 'development');
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
-const {series, parallel, src, dest, watch, lastRun} = require('gulp');
-const fs            = require('fs');
-const plumber       = require('gulp-plumber');
-const del           = require('del');
-const pug           = require('gulp-pug');
-const rename        = require('gulp-rename');
-const browserSync   = require('browser-sync').create();
-const sass          = require('gulp-sass');
-const postcss       = require('gulp-postcss');
-const autoprefixer  = require('autoprefixer');
-const mqpacker      = require('css-mqpacker');
-const inlineSVG     = require('postcss-inline-svg');
-const cpy           = require('cpy');
-const csso          = require('gulp-csso');
-const debug         = require('gulp-debug');
+const { series, parallel, src, dest, watch, lastRun } = require('gulp');
+const fs = require('fs');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const pug = require('gulp-pug');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const inlineSVG = require('postcss-inline-svg');
+const cpy = require('cpy');
+const csso = require('gulp-csso');
+const debug = require('gulp-debug');
 const webpackStream = require('webpack-stream');
-const svgstore      = require('gulp-svgstore');
-const svgmin        = require('gulp-svgmin');
-const prettyHtml    = require('gulp-pretty-html');
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
+const prettyHtml = require('gulp-pretty-html');
 
-const config        = require('./config');
+const webpack = require('webpack');
 
-const webpack       = require('webpack');
+const gutil = require('gulp-util');
+const notify = require('gulp-notify');
+
 const webpackConfig = require('./webpack.config').createConfig;
-const gutil         = require('gulp-util');
-const notify        = require('gulp-notify');
+const config = require('./config');
 
 // Настройки бьютификатора
 const prettyOption = {
   indent_size: 2,
   indent_char: ' ',
   unformatted: ['code', 'em', 'strong', 'span', 'i', 'b', 'br', 'script'],
-  content_unformatted: [],
+  content_unformatted: []
 };
 
 // Список и настройки плагинов postCSS
@@ -44,16 +45,35 @@ const postCssPlugins = [
   inlineSVG()
 ];
 
+/**
+ * Проверка существования файла или папки
+ * @param  {string} path      Путь до файла или папки
+ * @return {boolean}
+ */
+function fileExist(filepath) {
+  let flag = true;
+  try {
+    fs.accessSync(filepath, fs.F_OK);
+  } catch (e) {
+    flag = false;
+  }
+  return flag;
+}
+
 // Pug
 function compilePug() {
-  return src([config.src.templates + '/**/*.pug'], {since: lastRun(compilePug)})
-    .pipe(plumber({
-      errorHandler: function (err) {
-        console.log(err.message);
-        this.emit('end');
-      }
-    }))
-    .pipe(debug({title: 'Compiles '}))
+  return src(`${config.src.templates}/**/*.pug`, {
+    since: lastRun(compilePug)
+  })
+    .pipe(
+      plumber({
+        errorHandler: function(err) {
+          console.log(err.message);
+          this.emit('end');
+        }
+      })
+    )
+    .pipe(debug({ title: 'Compiles ' }))
     .pipe(pug())
     .pipe(prettyHtml(prettyOption))
     .pipe(dest(config.dest.html));
@@ -62,25 +82,24 @@ exports.compilePug = compilePug;
 
 // Copy Assets
 function copyAssets(cb) {
-  for (let item in config.addAssets) {
-    let dest = `${config.dest.root}/${config.addAssets[item]}`;
-    cpy(item, dest);
-  }
+  Object.keys(config.addAssets).forEach(key => {
+    cpy(key, `${config.dest.root}${config.addAssets[key]}`);
+  });
+
   cb();
 }
 exports.copyAssets = copyAssets;
 
 // Copy images
 function copyImg(cb) {
-  let copiedImages = [];
+  const copiedImages = [];
 
   if (copiedImages.length) {
     (async () => {
       await cpy(copiedImages, config.desc.img);
       cb();
     })();
-  }
-  else {
+  } else {
     cb();
   }
 }
@@ -88,57 +107,68 @@ exports.copyImg = copyImg;
 
 // Generate SVG Sprite
 function generateSvgSprite(cb) {
-  const spriteSvgPath = config.src.icons
+  const spriteSvgPath = config.src.icons;
 
   if (fileExist(spriteSvgPath)) {
-    return src(config.src.iconsSvg + '/*.svg')
-    .pipe(svgmin(function () {
-      return { plugins: [{ cleanupIDs: { minify: true } }] }
-    }))
-    .pipe(svgstore({ inlineSvg: true }))
-    .pipe(rename('sprite.svg'))
-    .pipe(dest(config.dest.img));
-  } else {
-    cb();
+    return src(`${config.src.iconsSvg}/*.svg`)
+      .pipe(
+        svgmin(function() {
+          return { plugins: [{ cleanupIDs: { minify: true } }] };
+        })
+      )
+      .pipe(svgstore({ inlineSvg: true }))
+      .pipe(rename('sprite.svg'))
+      .pipe(dest(config.dest.img));
   }
+
+  return cb();
 }
 exports.generateSvgSprite = generateSvgSprite;
 
 // Compile SASS
 function compileSass() {
-  return src(config.src.sass + '/*.{sass,scss}', {sourcemaps: true})
-    .pipe(sass({
+  return src(`${config.src.sass}/*.{sass,scss}`, { sourcemaps: true })
+    .pipe(
+      sass({
         outputStyle: config.production ? 'compact' : 'expanded', // nested, expanded, compact, compressed
         precision: 5
-    }))
+      })
+    )
     .on('error', config.errorHandler)
     .pipe(postcss(postCssPlugins))
-    .pipe(csso({
-      restructure: false,
-    }))
-    .pipe(dest(config.dest.css, {sourcemaps: '.'}))
+    .pipe(
+      csso({
+        restructure: false
+      })
+    )
+    .pipe(dest(config.dest.css, { sourcemaps: '.' }))
     .pipe(browserSync.stream());
 }
 exports.compileSass = compileSass;
 
 // Webpack log handler
 function handler(err, stats, cb) {
-  const errors = stats.compilation.errors;
+  const { errors } = stats.compilation;
 
   if (err) throw new gutil.PluginError('webpack', err);
 
   if (errors.length > 0) {
-    notify.onError({
-      title: 'Webpack Error',
-      message: '<%= error.message %>',
-      sound: 'Submarine'
-    }).call(null, errors[0]);
+    notify
+      .onError({
+        title: 'Webpack Error',
+        message: '<%= error.message %>',
+        sound: 'Submarine'
+      })
+      .call(null, errors[0]);
   }
 
-  gutil.log('[webpack]', stats.toString({
-    colors: true,
-    chunks: false
-  }));
+  gutil.log(
+    '[webpack]',
+    stats.toString({
+      colors: true,
+      chunks: false
+    })
+  );
 
   browserSync.reload();
   if (typeof cb === 'function') cb();
@@ -146,9 +176,11 @@ function handler(err, stats, cb) {
 
 function buildJs() {
   return src('src/js/app.js')
-    .pipe(webpackStream(webpackConfig(config.env), webpack, function(err, stats) {
-      handler(err, stats);
-    }))
+    .pipe(
+      webpackStream(webpackConfig(config.env), webpack, function(err, stats) {
+        handler(err, stats);
+      })
+    )
     .pipe(dest(config.dest.js));
 }
 exports.buildJs = buildJs;
@@ -166,47 +198,60 @@ function reload(done) {
 function serve() {
   browserSync.init({
     server: {
-      baseDir: !config.production ? [config.dest.root, config.src.root] : config.dest.root,
+      baseDir: !config.production
+        ? [config.dest.root, config.src.root]
+        : config.dest.root,
       directory: false,
       serveStaticOptions: {
         extensions: ['html']
       }
     },
     files: [
-      config.dest.html + '/*.html',
-      config.dest.css + '/*.css',
-      config.dest.img + '/**/*'
+      `${config.dest.html}/*.html`,
+      `${config.dest.css}/*.css`,
+      `${config.dest.img}/**/`
     ],
     port: 8080,
     open: false,
     notify: false
   });
 
-  watch([
-    config.src.templates + '/**/*.pug'
-  ], { events: ['change', 'add'], delay: 100 }, series(compilePug, reload));
+  watch(
+    `${config.src.templates}/**/*.pug`,
+    { events: ['change', 'add'], delay: 100 },
+    series(compilePug, reload)
+  );
 
-  watch(config.src.img + '/*', { events: ['all'], delay: 100 }, series(
-    copyImg,
-    reload
-  ));
+  watch(
+    `${config.src.img}/*`,
+    { events: ['all'], delay: 100 },
+    series(copyImg, reload)
+  );
 
-  watch(config.src.js + '/**/*.js', { events: ['all'], delay: 100 }, series(buildJs, reload));
+  watch(
+    `${config.src.js}/**/*.js`,
+    { events: ['all'], delay: 100 },
+    series(buildJs, reload)
+  );
 
-  watch(config.src.sass + '/**/*.{sass,scss}', { events: ['all'], delay: 100 }, series(compileSass, reload));
+  watch(
+    `${config.src.sass}/**/*.{sass,scss}`,
+    { events: ['all'], delay: 100 },
+    series(compileSass, reload)
+  );
 
-  watch([`${config.src.icons}/*.svg`], { events: ['all'], delay: 100 }, series(
-    generateSvgSprite,
-    copyImg,
-    reload
-  ));
+  watch(
+    [`${config.src.icons}/*.svg`],
+    { events: ['all'], delay: 100 },
+    series(generateSvgSprite, copyImg, reload)
+  );
 }
 
 exports.build = series(
   parallel(clearBuildDir),
   parallel(compilePug, copyAssets, generateSvgSprite),
   parallel(copyImg),
-  parallel(compileSass, buildJs),
+  parallel(compileSass, buildJs)
 );
 
 exports.default = series(
@@ -214,20 +259,5 @@ exports.default = series(
   parallel(compilePug, copyAssets, generateSvgSprite),
   parallel(copyImg),
   parallel(compileSass, buildJs),
-  serve,
+  serve
 );
-
-/**
- * Проверка существования файла или папки
- * @param  {string} path      Путь до файла или папки
- * @return {boolean}
- */
-function fileExist(filepath) {
-  let flag = true;
-  try {
-    fs.accessSync(filepath, fs.F_OK);
-  } catch(e) {
-    flag = false;
-  }
-  return flag;
-}
